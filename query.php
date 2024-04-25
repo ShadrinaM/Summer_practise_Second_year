d98964d5cd789e40df76e2dd24dc2dc6744210b8
BusinaLapusina <busina_lapusina@mail.ru>
2024-04-25 23:16:32 +0300

    fgdfg

--- query.php
+++ query.php
@@ -1,14 +1,13 @@
 <?php
-include '../ShadrinaMM_Web2/Secret.php';
-$user = 'userr';
-$pass = 'passs';
-
-$query = $_POST['query'];
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include ('../ShadrinaMM_Web2/Secret.php');
+$user = userr;
+$pass = passs;
+$db = new PDO(
+    "mysql:host=localhost;dbname=$user",
+    $user,
+    $pass,
+    [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+);
 
 if ($query == 1) {
     $sql = "SELECT * FROM Apartments WHERE Rooms_Count = 3 AND Street = 'Садовая'";
